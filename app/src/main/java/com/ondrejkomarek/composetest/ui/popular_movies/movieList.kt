2516104a1f74d2cae063929662667f9933d2f3ae
Ondrej Komarek <ondrej.komarek@strv.com>
2022-01-05 18:44:46 +0100

    Fixed weird scroll issue in popular movie list, tweaked image loading there.

--- app/src/main/java/com/ondrejkomarek/composetest/ui/popular_movies/movieList.kt
+++ app/src/main/java/com/ondrejkomarek/composetest/ui/popular_movies/movieList.kt
@@ -1,12 +1,14 @@
 package com.ondrejkomarek.composetest.ui.popular_movies
 
+import android.graphics.drawable.Drawable
 import android.os.Handler
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -16,18 +18,24 @@ import androidx.compose.material.icons.filled.WbSunny
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterStart
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
-import coil.request.ImageRequest
-import coil.transform.RoundedCornersTransformation
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.ondrejkomarek.composetest.R
 import com.ondrejkomarek.composetest.model.Movie
 import com.ondrejkomarek.composetest.network.MovieRepository
 import com.ondrejkomarek.composetest.ui.BaseViewModel
@@ -38,6 +46,10 @@ import com.ondrejkomarek.composetest.ui.universal.MyCircularProgressIndicator
 import com.ondrejkomarek.composetest.utility.Failure
 import com.ondrejkomarek.composetest.utility.ScreenState
 import com.ondrejkomarek.composetest.utility.fold
+import com.ondrejkomarek.composetest.utility.toPx
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.rememberDrawablePainter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -78,7 +90,12 @@ fun PopularMovies(
 								focusedIndicatorColor = MaterialTheme.colors.primaryVariant,
 								focusedLabelColor = MaterialTheme.colors.primaryVariant
 							),
-							placeholder = { Text( text = "Search movies", color = Color.White.copy(alpha = 0.5f)) }
+							placeholder = {
+								Text(
+									text = "Search movies",
+									color = Color.White.copy(alpha = 0.5f)
+								)
+							}
 						)
 					} else {
 						Text(text = "Popular movies")
@@ -145,20 +162,22 @@ fun PopularMoviesContent(
 	val scrollState = rememberLazyListState()
 
 	Column(modifier) {
-		LazyColumn(state = scrollState) {
-			items(viewState.movies.size, key = { viewState.movies[it].id }) {
-				val movieItem = viewState.movies[it]
-				if((searchText.isNotBlank() && movieItem.title.contains(
-						searchText,
-						ignoreCase = true
-					)) || searchText.isBlank()
-				) {
-					MovieListCard(movieItem, onPopularMovieClick)
+		if(viewState.movies.isNotEmpty()) {
+			LazyColumn(state = scrollState) {
+				items(items = viewState.movies) { movieItem ->
+					if((searchText.isNotBlank() && movieItem.title.contains(
+							searchText,
+							ignoreCase = true
+						)) || searchText.isBlank()
+					) {
+						MovieListCard(movieItem, onPopularMovieClick)
+					}
 				}
 			}
 		}
 
-		if(viewState.movies.map { it.title }.filter { it.contains(searchText, ignoreCase = true) }.isEmpty()) {
+		if(viewState.movies.map { it.title }.filter { it.contains(searchText, ignoreCase = true) }
+				.isEmpty()) {
 			EmptyState()
 		}
 	}
@@ -167,6 +186,18 @@ fun PopularMoviesContent(
 @ExperimentalCoilApi
 @Composable // for better reusability
 fun MovieListCard(movieItem: Movie, onMovieClick: (Movie) -> Unit, modifier: Modifier = Modifier) {
+	/*val resources = LocalContext.current.resources
+	val bitmap = remember {
+		val option = BitmapFactory.Options()
+		option.inPreferredConfig = Bitmap.Config.ARGB_8888
+		BitmapFactory.decodeResource(
+			resources,
+			R.drawable.trollface,
+			option
+		).asImageBitmap()
+	}*/
+
+
 	Card(
 		backgroundColor = MaterialTheme.colors.surface,
 		modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -177,25 +208,64 @@ fun MovieListCard(movieItem: Movie, onMovieClick: (Movie) -> Unit, modifier: Mod
 				.padding(16.dp)
 				.fillMaxWidth(1f)
 		) {
-			Surface(
+			Box(
 				modifier = Modifier
-					.height(120.dp),
-				color = MaterialTheme.colors.onSurface,
-				shape = RoundedCornerShape(12.dp)
+					.height(120.dp)
+					.wrapContentWidth(Start)
+				//color = MaterialTheme.colors.onSurface,
+				//shape = RoundedCornerShape(12.dp)
 			) {
-				Image(
-					painter = rememberImagePainter(
+				/*Image(
+					/*painter = rememberImagePainter(
 						data = movieItem.posterUrl,
 						builder = {
 							ImageRequest.Builder(LocalContext.current)
 								.transformations(RoundedCornersTransformation(12f))
 						}
-					),
+					),*/
+					//bitmap = bitmap,
+					painter = painterResource(id = R.drawable.trollface),
 					contentScale = ContentScale.Fit,
 					contentDescription = "Movie poster",
 					modifier = Modifier
 						.sizeIn(minHeight = 120.dp)
 						.fillMaxHeight(),
+				)*/
+
+				GlideImage(
+					imageModel = movieItem.posterUrl,
+					// Crop, Fit, Inside, FillHeight, FillWidth, None
+					contentScale = ContentScale.Fit,
+					alignment = CenterStart,
+					// shows an image with a circular revealed animation.
+					circularReveal = CircularReveal(duration = 500),
+					previewPlaceholder = R.drawable.trollface,  // does not work with success
+					requestOptions = {
+						RequestOptions()
+							.diskCacheStrategy(DiskCacheStrategy.ALL)
+							.transform(RoundedCorners(48.dp.toPx.toInt()))
+					},
+					modifier = Modifier
+						//.fillMaxHeight()
+						//.wrapContentHeight(Alignment.Top, false)//Alignment.Top, true)
+						//.defaultMinSize(Dp.Hairline, 1.dp)
+						.height(120.dp)
+						//.width(90.dp)
+						.wrapContentWidth(Start)
+						.align(CenterStart)
+						//.background(MaterialTheme.colors.primaryVariant)
+					,
+					// TODO adding success composable which actually draws drawable works - it shows properly scaled image, but circular reveal ofc does not work. Commenting this out will enable it, but screws up width scaling.
+					success = {
+						Image(
+							painter = rememberDrawablePainter(it.drawable),
+							contentScale = ContentScale.Fit,
+							contentDescription = "Movie poster",
+							modifier = Modifier
+								.height(120.dp)
+								.wrapContentWidth(Start)
+						)
+					}
 				)
 			}
 			Column(
